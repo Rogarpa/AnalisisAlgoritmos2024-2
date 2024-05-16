59d9815bbaa1915c83d2e8c037833148eafce595
Rodriginsky <rodrigoestrategia2000@gmail.com>
2024-05-15 22:07:59 -0600

    [feat]main:Add data structur bilinked list

--- P3/main.py
+++ P3/main.py
@@ -1,7 +1,7 @@
 class SortingAlgorithms:
 
     def LocalInsertionSort(self, array):
-
+        
         return "LocalInsertionSort"
 
     def TreeSort(self, array):
@@ -18,6 +18,39 @@ class SortingAlgorithms:
     def RadixLSDSort(self, array):
         print(array)
 
+class bilinked_list_node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.before = None
+
+    def insert_before(self, data):
+        node = bilinked_list_node(data)
+        old_before = self.before
+        self.before = node
+        if(old_before != None):
+            old_before.next = node
+        node.before = old_before
+        node.next = self
+
+    def insert_after(self, data):
+        node = bilinked_list_node(data)
+        old_next = self.next
+        self.next = node
+        if(old_next != None):
+            old_next.before = node
+        node.before = self
+        node.next = old_next
+    
+    def toArray(self, node):
+        resultArray = []
+        if(node == None):
+            return resultArray
+        if(node.data != None):
+            resultArray.append(node.data)
+        if(node.next != None):
+            resultArray = resultArray + self.toArray(node.next)
+        return resultArray
 
 class tree_node:
     def __init__(self, data):
@@ -57,4 +90,8 @@ class tree_node:
 # sorting.LocalInsertionSort(array)
 # print(sorting.TreeSort(array))
 # sorting.RadixLSDSort(array)
-node = bilinked_list_node()
+head = bilinked_list_node(1)
+head.insert_after(5)
+head.insert_after(4)
+head.insert_after(3)
+print(head.toArray(head))
